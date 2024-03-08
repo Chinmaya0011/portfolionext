88ef6e1fda0af6010d589008a944a3c29dcc4dca
Chinmaya0011 <imchinu17@gmail.com>
2024-03-08 21:14:10 +0530

    Updated Contact component and CSS

--- src/app/Components/Contact.js
+++ src/app/Components/Contact.js
@@ -1,77 +1,22 @@
-"use client"
 import React, { useState } from 'react';
 import styles from '../styles/contact.module.css'; // Importing CSS module
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can handle form submission here, for example, send the data to a server
-    console.log(formData);
-    // Reset form fields after submission
-    setFormData({
-      name: '',
-      email: '',
-      message: ''
-    });
-  };
-
+  
   return (
-    <div className={styles.contact}>      <h2>Contact</h2>
-    <div className={styles.contactcard}>
-
-      <form onSubmit={handleSubmit}>
-        <div className={styles.inputField}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </div>
-        <div className={styles.inputField}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </div>
-        <div className={styles.inputField}>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-            className={styles.textarea}
-          />
-        </div>
-        <button type="submit" className={styles.submitButton}>Submit</button>
-      </form>
-    </div>
+    <div className={styles.contact}>
+      <h2>Contact</h2>
+  
+      
+      {/* Google Form iframe */}
+      <div className={styles.googleFormContainer}>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeRanyD-E9tTC1ndKpysEY79xxG84E7_SL5I2PSJMOqHqjfwg/viewform?embedded=true"
+         
+        >
+          Loading…
+        </iframe>
+      </div>
     </div>
   );
 };
